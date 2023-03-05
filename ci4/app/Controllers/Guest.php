f501539015aa06a7c133a67a41032bdea63a4c9a
ldlaigo <ldlaigo@student.apc.edu.ph>
2023-03-06 02:55:06 +0800

    ci4

--- ci4/app/Controllers/Guest.php
+++ ci4/app/Controllers/Guest.php
@@ -1,10 +1,4 @@
-<div class="topnav">
-  <a class ="icon" href="home"><img src="https://64.media.tumblr.com/e0b978612c68d83635151d03fa995a0c/e1089a909f72b296-c7/s540x810/640bd85b2a8cc63ab5d08ee11459fec87c3a681a.jpg" style="width:48px;height:48px; margin:0px; margin-bottom:-5px;"></a>
-  <a href="home">Home</a>
-  <a class="active" href="news">SIMP CONFESSIONS</a>
-  <a href="create">CONFESS!</a>
-</div>
-<div class = "divbody">
+
 <?php
 namespace App\Controllers;
 use App\Models\GuestModel;
@@ -84,3 +78,10 @@ class Guest extends BaseController
     }
     
 }
+<div class="topnav">
+  <a class ="icon" href="home"><img src="https://64.media.tumblr.com/e0b978612c68d83635151d03fa995a0c/e1089a909f72b296-c7/s540x810/640bd85b2a8cc63ab5d08ee11459fec87c3a681a.jpg" style="width:48px;height:48px; margin:0px; margin-bottom:-5px;"></a>
+  <a href="home">Home</a>
+  <a class="active" href="news">SIMP CONFESSIONS</a>
+  <a href="create">CONFESS!</a>
+</div>
+<div class = "divbody">
